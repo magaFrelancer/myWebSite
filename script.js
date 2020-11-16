be36cfbd3ab015c8b68aa789bcfebcfdc1fa740c
magaFrelancer <ban0le4@gmail.com>
2020-11-16 21:06:26 +0300

    slider

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+//languege
 const languageNow = document.querySelector('.language__now');
 const langOther = document.querySelector('.language__other');
-languageNow.addEventListener('click', ({
-    target
-}) => {
-    langOther.classList.toggle('active')
-    target.closest('.language__now')
-        .querySelector('i').classList.toggle('active')
+languageNow.addEventListener('click', () => {
+    langOther.classList.toggle('active');
+    languageNow.querySelector('i').classList.toggle('active');
 })
+
+//slider
+
+const img = document.querySelectorAll('.slider__imgs-item');
+let index = 0;
+
+setInterval(() => {
+    img.forEach(item => item.classList.remove('active'))
+    index++;
+    if (index > img.length - 1) index = 0;
+    else {
+        index = img.length - 1;
+    }
+    img[index].classList.add('active')
+}, 3000);
